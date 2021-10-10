6dba005ae835161f80ddefa8b6e92d69c93ae36a
kudrchristina <53395252+christina-a-11@users.noreply.github.com>
2021-10-10 19:38:59 +0300

    update
    update

--- Japanesse_crossword/main.cpp
+++ Japanesse_crossword/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 void print(int nonogram[41][41], int n, int m);
 bool color_the_picture(int string[41][11], int n, int column[11][41], int m, int nonogram[41][41], int k);
-int number_of_ways(int i, int string[41][11], int n);
-void put_i_way(int i, int string[41][11], int n, int nonogram[41][41]);
+int number_of_ways(int ii, int string[41][11], int n);
+void put_i_way(int ii, int string[41][11], int count, int n, int nonogram[41][41]);
 void delete_i_way(int ii, int nonogram[41][41], int m);
 bool checking_by_columns(int i, int column[11][41], int m, int string[41][11], int n);
 
@@ -52,7 +52,7 @@ bool color_the_picture(int string[41][11], int n, int column[11][41], int m, int
     }
     for(int i = 1; i <= number_of_ways(n - k, string, n); i++)
     {
-        put_i_way(i, string, n, nonogram);
+        put_i_way(i, string, n - k, n, nonogram);
         if(checking_by_columns(i, column, m, string, n))
         {
             return color_the_picture(string, n, column, m, nonogram, k - 1);
@@ -67,20 +67,76 @@ bool color_the_picture(int string[41][11], int n, int column[11][41], int m, int
 }
 
 //считает кол-во перестановок групп
-int number_of_ways(int i, int string[41][11], int n)
+int number_of_ways(int ii, int string[41][11], int n)
 {
     int ans = 0;
+    int str[41]{-1};
+    int k = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int i = 1; i <= string[ii][0]; i++)
+        {
+            for(int j = 0; j < string[ii][i]; j++)
+            {
+                if(k > n)
+                {
+                    break;
+                }
+                str[k] = 1;
+                if(j == string[ii][i] - 1 && i != string[ii][0])
+                {
+                    str[k + 1] = -1;
+                    k++;
+                }
+            }
+            if(i = string[ii][0])
+            {
+                ans++;
+            }
+        }
+    }
     return ans;
 }
 
 //вставляет в нонограму расстановку по номеру i
-void put_i_way(int i, int string[41][11], int n, int nonogram[41][41])
+void put_i_way(int ii, int string[41][11], int count, int n, int nonogram[41][41])
 {
-
+    int str[41]{-1};
+    int k = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int i = 1; i <= string[ii][0]; i++)
+        {
+            for(int j = 0; j < string[ii][i]; j++)
+            {
+                if(k > n)
+                {
+                    break;
+                }
+                str[k] = 1;
+                if(j == string[ii][i] - 1 && i != string[ii][0])
+                {
+                    str[k + 1] = -1;
+                    k++;
+                }
+            }
+            if(i = string[ii][0])
+            {
+                count--;
+            }
+            if(count == 0)
+            {
+                for(int i = 0; i < n; i++)
+                {
+                    nonogram[ii][i] = str[i];
+                }
+                return;
+            }
+        }
+    }
 }
-
 //проверят строчную расстановку на соответсвие столбикам
-bool checking_by_columns(int i, int column[11][11], int m, int string[41][11], int n)
+bool checking_by_columns(int i, int column[11][41], int m, int string[41][11], int n)
 {
     return true;
 }
@@ -108,7 +164,7 @@ int main()
     {
         int a = 0;
         cin >> a;
-        column[i][0] = a;
+        column[0][i] = a;
         for(int j = 1; j < a; j++)
         {
             cin >> column[j][i];
