0de4a9b9b09db44a9902a5877062e80b44716158
kudrchristina <53395252+christina-a-11@users.noreply.github.com>
2021-10-21 11:54:35 +0300

    add reverse polish notation
    add reverse polish notation

---
+++ reverse_polish_notation/main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+int f(int a, int b, char x)
+{
+	switch (x)
+	{
+	case '+':
+		return a + b;
+	case '-':
+		return b - a;
+	case '/':
+		return b / a;
+	case '*':
+		return a * b;
+	}
+}
+int f2()
+{
+	stack <int> arr;
+	char x = '0';
+	while (cin >> x)
+	{
+		if (isdigit(x))
+		{
+			cin.putback(x);
+			int n = 0;
+			cin >> n;
+			arr.push(n);
+		}
+		else
+		{
+			int a = 0;
+			int b = 0;
+			a = arr.top();
+			arr.pop();
+			b = arr.top();
+			arr.pop();
+			arr.push(f(a, b, x));
+		}
+	}
+	return arr.top();
+}
+void print(vector<char> a)
+{
+	for (auto x : a)
+	{
+		cout << x << " ";
+	}
+}
+
+void f1()
+{
+	stack <char> arr;
+	vector <char> ans;
+	char x;
+	while (cin >> x)
+	{
+		if (isdigit(x))
+		{
+			ans.push_back(x);
+		}
+		else
+		{
+			if (x == '(')
+			{
+				arr.push(x);
+			}
+			else if (x == ')')
+			{
+				while (arr.top() != '(')
+				{
+					ans.push_back(arr.top());
+					arr.pop();
+				}
+				arr.pop();
+			}
+			else
+			{
+				if (!arr.empty())
+				{
+					if (arr.top() != '(')
+					{
+						if (x == '/' || x == '*')
+						{
+							while (arr.top() != '+' && arr.top() != '-')
+							{
+								ans.push_back(arr.top());
+								arr.pop();
+							}
+						}
+						else if (x == '+' || x == '-')
+						{
+							while (!arr.empty())
+							{
+								ans.push_back(arr.top());
+								arr.pop();
+							}
+						}
+					}
+				}
+				arr.push(x);
+			}
+		}
+	}
+	while (!arr.empty())
+	{
+		ans.push_back(arr.top());
+		arr.pop();
+	}
+	print(ans);
+}
+int main()
+{
+	cout << f2();
+  f1();
+	return 0;
+}
